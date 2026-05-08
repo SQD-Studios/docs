459178ddd000cfb4116e4a322035992307c0b0d7
Chamogelastos <stavros.ssta@gmail.com>
2026-05-08 20:52:45 +0300

    Actually delete all files

--- .astro/content-modules.mjs
+++
@@ -1,6 +0,0 @@
-
-export default new Map([
-["src/content/docs/ChamoOrders/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2FChamoOrders%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/ChamoOrders/Introduction/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2FChamoOrders%2FIntroduction%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")]]);
-		
