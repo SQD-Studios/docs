f6dec2f4040e2aa7938c9cf84a809cc5c71d64d7
Chamogelastos <stavros.ssta@gmail.com>
2026-07-19 16:38:42 +0300

    What did I do for a javadoc button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 import tailwindcss from '@tailwindcss/vite';
@@ -9,94 +9,97 @@ import starlightSidebarTopics from 'starlight-sidebar-topics'
 export default defineConfig({
     site: 'https://sqdstudios.github.io',
     base: 'docs/',
-  integrations: [
+    integrations: [
 
-      starlight({
-          favicon: 'favicon.ico',
-          plugins: [
-              starlightSidebarTopics([
-                  {
-                      label: 'ABX Velocity',
-                      link: 'abxvelocity/administration/introduction/installing',
-                      icon: 'open-book',
-                      items: [
-                          {
-                              label: "Administration",
-                              items: [
-                                  {
-                                      label: "Introduction",
-                                      items: ['abxvelocity/administration/introduction/installing'],
-                                  },
+        starlight({
+            favicon: 'favicon.ico',
+            plugins: [
+                starlightSidebarTopics([
+                        {
+                            label: 'ABX Velocity',
+                            link: '/abxvelocity/administration/introduction/installing',
+                            icon: 'open-book',
+                            items: [
+                                {
+                                    label: "Administration",
+                                    items: [
+                                        {
+                                            label: "Introduction",
+                                            items: ['abxvelocity/administration/introduction/installing'],
+                                        },
 
-                              ]
-                          },
-                      ],
-                  },
-                  {
-                      label: 'ChamoParty',
-                      link: 'chamoparty/administration/introduction/installing',
-                      icon: 'information',
-                      items: [
-                          {
-                              label: "Administration",
-                              items: [
-                                  {
-                                      label: "Introduction",
-                                      items: ['chamoparty/administration/introduction/installing'],
-                                  },
+                                    ]
+                                },
+                            ],
+                        },
+                        {
+                            label: 'ChamoParty',
+                            link: '/chamoparty/administration/introduction/installing',
+                            icon: 'information',
+                            items: [
+                                {
+                                    label: "Administration",
+                                    items: [
+                                        {
+                                            label: "Introduction",
+                                            items: ['chamoparty/administration/introduction/installing'],
+                                        },
 
-                              ]
-                          },
-                      ],
-                  },
-                  {
-                      label: 'ChamoItemSkins',
-                      link: 'chamoitemskins/administration/introduction/installing',
-                      icon: 'inventory',
-                      items: [
-                          {
-                              label: "Administration",
-                              items: [
-                                  {
-                                      label: "Introduction",
-                                      items: ['chamoitemskins/administration/introduction/installing'],
-                                  },
+                                    ]
+                                },
+                            ],
+                        },
+                        {
+                            label: 'ChamoItemSkins',
+                            link: '/chamoitemskins/administration/introduction/installing',
+                            icon: 'inventory',
+                            items: [
+                                {
+                                    label: "Administration",
+                                    items: [
+                                        {
+                                            label: "Introduction",
+                                            items: ['chamoitemskins/administration/introduction/installing'],
+                                        },
 
-                              ]
-                          },
-                      ],
-                  },
-              ],
-                  {
-                      exclude: ['/nu/javadocs/selection']
-                  }
-                  ),
+                                    ]
+                                },
+                            ],
+                        },
 
-          ],
-          title: 'SQD Studios Documentation',
-          components: {
-              Footer: './src/components/overrides/Footer.astro',
-              Banner: './src/components/overrides/Banner.astro',
-              Sidebar: './src/components/Sidebar.astro',
-          },
-          editLink: {
-              baseUrl: 'https://github.com/SQD-Studios/docs/tree/master/',
-          },
-          tableOfContents: { minHeadingLevel: 1, maxHeadingLevel: 3 },
-          customCss: [
-              // Path to your Tailwind base styles:
-              './src/styles/global.css',
-              './src/fonts/font-face.css',
-          ],
-          logo: {
-          src: './src/assets/SQDStudios.webp',
-          },
-          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/SQD-Studios/'}, {icon: 'seti:java', label: 'Javados', href: 'nu/javadocs/selection'},],
+                    ], {
+                    exclude: ['/nu/javadocs/**/*']
+                    }
+                )
+            ],
+            title: 'SQD Studios Documentation',
+            components: {
+                Footer: './src/components/overrides/Footer.astro',
+                Banner: './src/components/overrides/Banner.astro',
+                Sidebar: './src/components/Sidebar.astro',
+            },
+            editLink: {
+                baseUrl: 'https://github.com/SQD-Studios/docs/tree/master/',
+            },
+            tableOfContents: {minHeadingLevel: 1, maxHeadingLevel: 3},
+            customCss: [
+                // Path to your Tailwind base styles:
+                './src/styles/global.css',
+                './src/fonts/font-face.css',
+            ],
+            logo: {
+                src: './src/assets/SQDStudios.webp',
+            },
+            social: [{icon: 'github', label: 'GitHub', href: 'https://github.com/SQD-Studios/'}, {
+                icon: 'seti:java',
+                label: 'Javados',
+                href: '/docs/nu/javadocs/selection'
+            },],
 
-      }),
-	],
+        }),
+    ],
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,7 @@
 import { defineCollection } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
+import { topicSchema } from 'starlight-sidebar-topics/schema'
 
 export const collections = {
 	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
